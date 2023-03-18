32d9507ad402afec6b7c877da806d272b2fd123f
Lina Hammami <hamemilina@gmail.com>
2023-03-18 01:13:02 +0100

    Adding Claim Model

---
+++ claims/claims/src/main/java/com/avidtrain/claims/models/Claim.java
@@ -0,0 +1,58 @@
+package com.avidtrain.claims.models;
+
+import java.util.Date;
+
+public class Claim {
+    enum Status{
+        OPEN,
+        EXPERTISE,
+        CLOSED
+    };
+    private String claimId;
+    private String claimNb;
+    private Status status;
+    private Date accidentDate;
+    private Date creationDate;
+
+    public String getClaimId() {
+        return claimId;
+    }
+
+    public void setClaimId(String claimId) {
+        this.claimId = claimId;
+    }
+
+    public String getClaimNb() {
+        return claimNb;
+    }
+
+    public void setClaimNb(String claimNb) {
+        this.claimNb = claimNb;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getAccidentDate() {
+        return accidentDate;
+    }
+
+    public void setAccidentDate(Date accidentDate) {
+        this.accidentDate = accidentDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+
+}
